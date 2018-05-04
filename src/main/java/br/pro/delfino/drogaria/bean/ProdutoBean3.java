21a5c526d46f16dc8b5ae6612cc71a61269bf012
CleberGraciano <cleber_batista2010@hotmail.com>
2018-05-04 17:08:52 -0300

    Desmenbrando a tela de Produtos

---
+++ src/main/java/br/pro/delfino/drogaria/bean/ProdutoBean3.java
@@ -0,0 +1,53 @@
+package br.pro.delfino.drogaria.bean;
+
+import java.io.Serializable;
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+
+import org.omnifaces.util.Messages;
+
+import br.pro.delfino.drogaria.dao.ProdutoDao;
+import br.pro.delfino.drogaria.domain.Produto;
+
+@SuppressWarnings("serial")
+@ManagedBean
+@ViewScoped
+public class ProdutoBean3 implements Serializable {
+	
+	private List<Produto> produtos;
+	
+	private ProdutoDao produtoDao;
+
+	public List<Produto> getProdutos() {
+		return produtos;
+	}
+
+	public void setProdutos(List<Produto> produtos) {
+		this.produtos = produtos;
+	}
+	
+	
+	@PostConstruct
+	public void iniciar() {
+		produtoDao = new ProdutoDao();
+		
+	}
+	
+	public void listar() {
+
+		try {
+
+			produtos = produtoDao.listar();
+
+		} catch (RuntimeException erro) {
+			Messages.addGlobalError("Ocorreu um erro ao tentar listar os Produtos");
+			erro.printStackTrace();
+		}
+
+	}
+	
+
+}
