807ed6c39bb8f5b9ba013eee9c3c437b1a8991de
CleberGraciano <cleber_batista2010@hotmail.com>
2018-05-04 17:28:11 -0300

    Fazendo tela de Produto Listagem Parei no curso na aula 286 - View Param

--- src/main/java/br/pro/delfino/drogaria/bean/ProdutoBean3.java
+++ src/main/java/br/pro/delfino/drogaria/bean/ProdutoBean3.java
@@ -9,7 +9,9 @@ import javax.faces.bean.ViewScoped;
 
 import org.omnifaces.util.Messages;
 
+import br.pro.delfino.drogaria.dao.FabricanteDao;
 import br.pro.delfino.drogaria.dao.ProdutoDao;
+import br.pro.delfino.drogaria.domain.Fabricante;
 import br.pro.delfino.drogaria.domain.Produto;
 
 @SuppressWarnings("serial")
@@ -17,9 +19,30 @@ import br.pro.delfino.drogaria.domain.Produto;
 @ViewScoped
 public class ProdutoBean3 implements Serializable {
 	
+	private Produto produto;
+	private List<Fabricante> fabricantes;
 	private List<Produto> produtos;
 	
+	private FabricanteDao fabricanteDao;
 	private ProdutoDao produtoDao;
+	
+	
+
+	public Produto getProduto() {
+		return produto;
+	}
+
+	public void setProduto(Produto produto) {
+		this.produto = produto;
+	}
+
+	public List<Fabricante> getFabricantes() {
+		return fabricantes;
+	}
+
+	public void setFabricantes(List<Fabricante> fabricantes) {
+		this.fabricantes = fabricantes;
+	}
 
 	public List<Produto> getProdutos() {
 		return produtos;
@@ -32,6 +55,7 @@ public class ProdutoBean3 implements Serializable {
 	
 	@PostConstruct
 	public void iniciar() {
+		fabricanteDao = new FabricanteDao();
 		produtoDao = new ProdutoDao();
 		
 	}
